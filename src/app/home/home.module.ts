13f882b87fb36ff7be788bc2995fdaafcc5c4bc6
sophiecra <sophieccrane@gmail.com>
2025-01-10 15:21:14 -0500

    add nav bar and pfp

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageContainerComponent } from './containers/home-page-container.component';
 import {HomeRoutingModule} from "./home-routing.module";
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 
 
 @NgModule({
   declarations: [
-    HomePageContainerComponent
+    HomePageContainerComponent,
+    NavBarComponent
   ],
   imports: [
     CommonModule, HomeRoutingModule
