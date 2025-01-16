6816c96c7b3cca3cdc0f8ae0fac05ff0d79a8a17
Sophie Crane <sophieccrane@gmail.com>
2025-01-16 11:11:31 -0500

    Feature: Add Basic Title Section and Nav Bar (#1)
    * Add title page and nav bar stylings
    * Build project

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'sans': ['"Monaco"', ...defaultTheme.fontFamily.sans],
+        'sans': ['ui-monospace', ...defaultTheme.fontFamily.sans],
       },
     },
   },
