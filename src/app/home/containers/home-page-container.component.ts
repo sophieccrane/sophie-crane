78fda180a3265ea0b030695429c9fee0fffb0c9a
Sophie Crane <sophieccrane@gmail.com>
2025-01-16 15:41:14 -0500

    Basic section addition and fragmentation changes

--- src/app/home/containers/home-page-container.component.ts
+++ src/app/home/containers/home-page-container.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-home-page-container',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-page-container.component.scss']
 })
 export class HomePageContainerComponent {
+
+  constructor(private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.activatedRoute.fragment.subscribe((fragment: string | null) => {
+      if (fragment) this.jumpToSection(fragment);
+    });
+  }
+
+  jumpToSection(section: string | null) {
+    if (section) document.getElementById(section)?.scrollIntoView({ behavior: 'smooth' });
+
+  }
 
 }
