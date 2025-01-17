1406f303ec49e5c38dacebbec94d74ec106d5e72
Sophie Crane <sophieccrane@gmail.com>
2025-01-17 10:45:27 -0500

    basic section buildout and responsive design for mobile

--- src/app/home/containers/home-page-container.component.ts
+++ src/app/home/containers/home-page-container.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import {Component, HostListener, Inject, PLATFORM_ID} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {DOCUMENT, isPlatformBrowser} from "@angular/common";
 
 @Component({
   selector: 'app-home-page-container',
@@ -7,18 +8,44 @@ import {ActivatedRoute} from "@angular/router";
   styleUrls: ['./home-page-container.component.scss']
 })
 export class HomePageContainerComponent {
+  receivedFragment: string | undefined;
 
-  constructor(private activatedRoute: ActivatedRoute) {}
+  constructor(private activatedRoute: ActivatedRoute,
+              private router: Router,
+              @Inject(PLATFORM_ID) private platformId: Object,
+              @Inject(DOCUMENT) private document: Document) {}
 
   ngOnInit() {
-    this.activatedRoute.fragment.subscribe((fragment: string | null) => {
-      if (fragment) this.jumpToSection(fragment);
-    });
+    this.jumpToSection("Home");
+  }
+
+  receiveFragment($event: string) {
+    this.receivedFragment = $event;
+    this.jumpToSection(this.receivedFragment);
   }
 
   jumpToSection(section: string | null) {
     if (section) document.getElementById(section)?.scrollIntoView({ behavior: 'smooth' });
 
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll() {
+    if (isPlatformBrowser(this.platformId)) {
+      const sections = this.document.querySelectorAll('section');
+      let currentSectionId = '';
+
+      sections.forEach(section => {
+        const rect = section.getBoundingClientRect();
+        if (rect.top <= 0 && rect.bottom > 0) {
+          currentSectionId = section.id;
+        }
+      });
+
+      if (currentSectionId) {
+        this.router.navigate([], { fragment : currentSectionId });
+      }
+    }
   }
 
 }
