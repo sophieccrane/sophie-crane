ff6ab994d178555b8447a03506b862d23924143b
Sophie Crane <sophieccrane@gmail.com>
2025-02-07 16:00:55 -0500

    Feature: Add Top Items to Spotify Section (#7)

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,4 +1,4 @@
 export const PLAYLIST_IDS = ['02ZQQyIVA1AhI1fOm1BhTU', '5Nn4LTfTT1ZnLyjwhLsvcm', '4SxBvxTu5INydoiX4uKTFj', '6Iqe2Go7EUZxDVGum1OK5U'];
 export const PLAYLIST_FIELDS_PARAM = 'external_urls,images,name,description';
-export const ARTIST_IDS = ['4dWtsQvuME6tCWFycaTvO7', '7FKTg75ADVMZgY3P9ZMRtH', '06HL4z0CvFAxyc27GXpf02', '7tYKF4w9nC0nq9CsPZTHyP', '74KM79TiuVKeVCqs8QtB0B', '3mIj9lX2MWuHmhNCA7LSCW', '1dfeR4HaWDbWqFHLkxsg1d'];
-export const TRACK_IDS = ['2E7a96qey4AzSOdK6H21vS', '35IcAVSMsU9qzHfpPbvC8A', '6AzCBeiDuUXGXjznBufswB', '12cHoMTM2zP1YCouDaG30s'];
+export const ARTIST_IDS = ['06HL4z0CvFAxyc27GXpf02', '3mIj9lX2MWuHmhNCA7LSCW', '4oUHIQIBe0LHzYfvXNW4QM', '74KM79TiuVKeVCqs8QtB0B', '7tYKF4w9nC0nq9CsPZTHyP', '6VuMaDnrHyPL1p4EHjYLi7', '1McMsnEElThX1knmY4oliG', '246dkjvS1zLTtiykXe5h60', '3IunaFjvNKj98JW89JYv9u', '6eJa3zG1QZLRB3xgRuyxbm'];
+export const TRACK_IDS = ['5N3hjp1WNayUPZrA8kJmJP', '2VwiNML5wi72bt0wUVDn8l', '2Zo1PcszsT9WQ0ANntJbID', '0ymhaRjiCwNQzeQEjewE7F', '102YUQbYmwdBXS7jwamI90', '21B4gaTWnTkuSh77iWEXdS', '5oIVNm56t6OIf9ZjdEG3ud', '71BqAINEnezjQfxE4VuJfq', '6dOtVTDdiauQNBQEDOtlAB', '36YY9Yeq3XastTeC1e0VmN'];
