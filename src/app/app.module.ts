2c8463e0074f012c3d521dc006d9bb84c1961c66
Sophie Crane <sophieccrane@gmail.com>
2025-03-14 14:21:30 -0400

    Feature: Add projects and technical skills to experience section (#10)
    * Add and run linter
    
    * Experience enhancements (projects, skills, etc)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HttpClient, HttpClientModule, HttpHandler} from "@angular/common/http";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
